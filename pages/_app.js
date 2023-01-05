641ac8fce5b1accdf0f526b36bb50235c207aace
Ferris Mohammed <ferris.mohammed@gmail.com>
2023-01-04 20:41:39 -0500

    Current Progress
    Added Some placeholder images, fine tunings the animations and Cursor at the TOP

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const threeScript = document.createElement("script");
+    threeScript.setAttribute("id", "threeScript");
+    threeScript.setAttribute(
+      "src",
+      "https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"
+    );
+    document.getElementsByTagName('head')[0].appendChild(threeScript)
+    return ()=>{
+      if(threeScript){
+        threeScript.remove()
+      }
+    }
+  },[]);
+  return <Component {...pageProps} />;
 }
